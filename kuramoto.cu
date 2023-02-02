64ec87b0f9c3a16a924e9b6b185c9ce1e77da478
Atish <atishpanday1@gmail.com>
2023-02-01 19:19:53 -0800

    added read me file explaining the kuramoto oscillators and rk4 method

--- kuramoto.cu
+++ kuramoto.cu
@@ -1,27 +1,3 @@
-/*
-kuramoto oscillators
-
-equation:
-	
-	d(theta[i])/dt = w[i] + lambda * sum( A[i][j] * sin(theta[j] - theta[i])
-	
-	solving the above using rk4 method:
-	
-	let df[i] = d(theta[i])/dt = f(theta[i]);
-	
-	then k1 = f(theta[i]);
-	
-	and k2 = f(theta[i] + h*k1/2)
-	
-	k3 = f(theta[i] + h*k2/2)
-	
-	k4 = f(theta[i] + h*k3)
-	
-	then we can find theta[i + 1] using the following formula:
-	
-	theta[i+1] = theta[i] + h*(k1 + 2*k2 + 2*k3 + k4) / 6
-*/
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
@@ -117,12 +93,13 @@ int main() {
 		iter++;
 	}
 	
+	cudaMemcpy(theta, dtheta, N * 10000 * sizeof(double), cudaMemcpyDeviceToHost);
+	
 	fclose(fptr_1);
 	fclose(fptr_2);
 	fclose(fptr_3);
 	
 	return 0;
-
 }
 
 
